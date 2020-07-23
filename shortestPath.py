c4b584ad2e21a2c2b1e2b583074d3722504f0b84
dxl2016 <60236777+dxl2016@users.noreply.github.com>
2020-07-23 17:02:06 -0400

    Update and rename shortest path.py to shortestPath.py

--- shortestPath.py
+++ shortestPath.py
@@ -1,14 +1,13 @@
-def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        
+def shortestPath(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
         graph = [[] for _ in range(n)]
         for p, (i,j) in enumerate(edges):
             graph[i].append([succProb[p],j])
             graph[j].append([succProb[p],i])
-        
+
         dist = collections.defaultdict(int)
         dist[start] = -1
         pq = [[-1,start]]
-        
+
         while(pq):
             curr_dist, curr_node = heapq.heappop(pq)
             for neigh_dist, neighbor in graph[curr_node]:
@@ -16,7 +15,6 @@ def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], 
                 if curr_prob < dist[neighbor]:
                     heapq.heappush(pq, [curr_prob,neighbor])
                     dist[neighbor] = curr_prob
-                    
-        
+
+
         return -dist[end]
-© 2020 GitHub, Inc.
