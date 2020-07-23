bb1dc94563e04d796042267229f782a4f3bb5d5b
dxl2016 <60236777+dxl2016@users.noreply.github.com>
2020-07-23 17:01:24 -0400

    Update myPow.py

--- myPow.py
+++ myPow.py
@@ -1,22 +1,21 @@
-class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        
-        if n < 0:
-            return 1 / self.myPow(x, -n)
-        else:      
-            p = 1
-            base = x
+def myPow(self, x, n):
+    """
+    :type x: float
+    :type n: int
+    :rtype: float
+    """
 
-            while n != 0:
-                if n % 2 == 1:
-                    p = p * base
+    if n < 0:
+        return 1 / self.myPow(x, -n)
+    else:      
+        p = 1
+        base = x
 
-                base *= base
-                n = n // 2
+        while n != 0:
+            if n % 2 == 1:
+                p = p * base
 
-            return p
+            base *= base
+            n = n // 2
+
+        return p
