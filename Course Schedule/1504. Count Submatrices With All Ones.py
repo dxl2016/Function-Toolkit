356c8f1d5355512d6f5a502c214fb0d523b87915
dxl2016 <60236777+dxl2016@users.noreply.github.com>
2020-07-26 12:21:54 -0400

    Create 1504. Count Submatrices With All Ones.py

---
+++ Course Schedule/1504. Count Submatrices With All Ones.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        m, n = len(mat), len(mat[0])
+        tmp = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n-1, -1, -1):
+                if j == n-1:
+                    tmp[i][j] = mat[i][j]
+                else:
+                    if mat[i][j] > 0:
+                        tmp[i][j] = tmp[i][j+1] + 1
+        # print(tmp)
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] > 0:
+                    width = sys.maxsize
+                    for k in range(i, m):
+                        width = min(width, tmp[k][j])
+                        ans += width
+        return ans
+
