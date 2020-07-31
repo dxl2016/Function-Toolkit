2fa135277f2bf0f3084b38c5177b61ab8caab6d2
dxl2016 <60236777+dxl2016@users.noreply.github.com>
2020-07-30 20:33:18 -0400

    Update 140. Word Break II.py

--- Word Search/140. Word Break II.py
+++ Word Search/140. Word Break II.py
@@ -5,25 +5,28 @@ class Solution(object):
         :type wordDict: Set[str]
         :rtype: List[str]
         """
-        return self.helper(s, wordDict, {})
+        def dfs(s, memo):
+            if not s:
+                return []
+            if s in memo:
+                return memo[s]
+            
+            res = []
+            for word in wordDict:
+                if not s.startswith(word):
+                    continue
+                if len(word) == len(s):
+                    res.append(word)
+                else:
+                    resultOfTheRest = dfs(s[len(word):], memo)
+                    for item in resultOfTheRest:
+                        item = word + ' ' + item
+                        res.append(item)
+            memo[s] = res
+            # print(memo)
 
-    def helper(self, s, wordDict, memo):
-        if s in memo: return memo[s]
-        if not s: return []
-
-        res = []
-        for word in wordDict:
-            if not s.startswith(word):
-                continue
-            if len(word) == len(s):
-                res.append(word)
-            else:
-                resultOfTheRest = self.helper(s[len(word):], wordDict, memo)
-                for item in resultOfTheRest:
-                    item = word + ' ' + item
-                    res.append(item)
-        memo[s] = res
-        print(memo)
-        
-        return res
-
+            return res
+    
+        return dfs(s, {})
+    
+    
