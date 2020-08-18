5a2879456c85f40e87b429058bce9b116df32cd2
dxl2016 <60236777+dxl2016@users.noreply.github.com>
2020-08-18 12:00:22 -0400

    Create 215. Kth Largest Element in an Array.py

---
+++ Classic Questions/215. Kth Largest Element in an Array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def partition(nums, l, r):
+            # print(nums)
+            low = l
+            while(l<r):
+                if nums[l] < nums[r]:
+                    nums[l], nums[low] = nums[low], nums[l]
+                    low += 1
+                l += 1
+            nums[low], nums[r] = nums[r], nums[low]
+            # print(nums)
+            return low
+    
+        def findKthSmallest(nums, k):
+            if nums:
+                pos = partition(nums, 0, len(nums)-1)
+                if k > pos+1:
+                    return findKthSmallest(nums[pos+1:], k-pos-1)
+                elif k < pos+1:
+                    return findKthSmallest(nums[:pos], k)
+                else:
+                    return nums[pos]
+
+        return findKthSmallest(nums, len(nums)+1-k)
+
